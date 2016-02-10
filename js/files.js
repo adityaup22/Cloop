6dc27c3daee491be6fc49c689efb8895907b336e
Gaurav Dhenwal <gauravdhenwal699@gmail.com>
2016-02-11 03:25:11 +0530

    files page
    values from xml file

--- js/files.js
+++ js/files.js
@@ -1,18 +1,41 @@
-var nn={
-'details':[
 
-{'Permissions' : 'drwxr-xr-x', 'Replication':'-', 'UploadedBy':'root', 'UserGroup':'supergroup', 'Size':'0', 'UploadedOn':'2015-08-12','FName':'/adaaaa'},
-{'Permissions' : 'drwxr-xr-x', 'Replication':'-', 'UploadedBy':'root', 'UserGroup':'supergroup', 'Size':'0', 'UploadedOn':'2015-08-12','FName':'/add'},
-{'Permissions' : '-rw-r--r--', 'Replication':'3', 'UploadedBy':'root', 'UserGroup':'supergroup', 'Size':'0', 'UploadedOn':'2015-08-12','FName':'/post.log'},
-{'Permissions' : 'drwxr-xr-x', 'Replication':'-', 'UploadedBy':'root', 'UserGroup':'supergroup', 'Size':'0', 'UploadedOn':'2015-08-12','FName':'/tmp'},
-{'Permissions' : 'drwxr-xr-x', 'Replication':'-', 'UploadedBy':'root', 'UserGroup':'supergroup', 'Size':'0', 'UploadedOn':'2015-08-12','FName':'/tmp/hadoop-root'},
-{'Permissions' : 'drwxr-xr-x', 'Replication':'-', 'UploadedBy':'root', 'UserGroup':'supergroup', 'Size':'0', 'UploadedOn':'2015-08-12','FName':'/tmp/hadoop-root/mapred'},
-{'Permissions' : 'drwx------', 'Replication':'-', 'UploadedBy':'root', 'UserGroup':'supergroup', 'Size':'0', 'UploadedOn':'2015-08-12','FName':'/tmp/hadoop-root/mapred/system'},
-{'Permissions' : '-rw-------', 'Replication':'3', 'UploadedBy':'root', 'UserGroup':'supergroup', 'Size':'4', 'UploadedOn':'2015-08-12','FName':'/tmp/hadoop-root/mapred/system/jobtracker.info'},
-{'Permissions' : 'drwxr-xr-x', 'Replication':'-', 'UploadedBy':'root', 'UserGroup':'supergroup', 'Size':'0', 'UploadedOn':'2015-08-12','FName':'/user'},
-{'Permissions' : 'drwxr-xr-x', 'Replication':'-', 'UploadedBy':'root', 'UserGroup':'supergroup', 'Size':'0', 'UploadedOn':'2015-08-12','FName':'/user/root'},
-{'Permissions' : 'drwxr-xr-x', 'Replication':'-', 'UploadedBy':'root', 'UserGroup':'supergroup', 'Size':'0', 'UploadedOn':'2015-08-12','FName':'/user/root/adaa'},
-]
+function loadthis() {
+	
+  var xhttp = new XMLHttpRequest();
+  
+  xhttp.onreadystatechange = function() {
+    if (xhttp.readyState == 4 && xhttp.status == 200) {
+     
+      myFunction(xhttp);
+    }
+  };
+	xhttp.open("GET","ajaxfiles/files.xml", true);//file managed by python dev
 
+  xhttp.send();
 }
- 
+
+
+
+
+function myFunction(xml) {
+  var i;
+  var xmlDoc = xml.responseXML;
+  var file = xmlDoc.getElementsByTagName("FILE");
+  
+	  
+	 
+  
+  var table="<tr><th>Name</th><th>Owner</th><th>Last Modified</th><th>Size</th></tr>";
+  
+  for (i = 0; i <file.length; i++) { 
+    table += "<tr><td >" +
+    file[i].getElementsByTagName("NAME")[0].childNodes[0].nodeValue +
+    "</td><td>" +
+    file[i].getElementsByTagName("OWNER")[0].childNodes[0].nodeValue + "</td><td>" +
+	file[i].getElementsByTagName("MODIFIED")[0].childNodes[0].nodeValue +"</td><td>" +
+	 file[i].getElementsByTagName("SIZE")[0].childNodes[0].nodeValue 
+  		}//end of for loop
+  document.getElementById("file").innerHTML = table;
+
+  
+}//------------------end of myFunction-------------//
