9104c34e1308132480698ef3b1cd3d00776b7f5c
Gaurav Dhenwal <gauravdhenwal699@gmail.com>
2016-02-20 11:09:03 +0530

    home new ... basic ...rejected

---
+++ homenew.php
@@ -0,0 +1,246 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+ <meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Home</title>
+<link rel="stylesheet" href="css/bootstrap.css" />
+<link rel='stylesheet' href='font-awesome/css/font-awesome.css'/>
+<link rel="stylesheet" href="css/homenew.css"/>
+<script src="js/jquery.js"></script>
+<script src="js/bootstrap.js"></script>
+<script src="js/homenew.js"></script>
+</head>
+
+<body id="myPage" data-spy="scroll" data-target=".navbar" data-offset="60">
+
+<nav class="navbar navbar-default navbar-fixed-top">
+  <div class="container">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span> 
+      </button>
+      <a class="navbar-brand" href="#">Infamous Cloud<i class="fa fa-cloud"></i></a>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="#about">ABOUT</a></li>
+        <li><a href="#services">SERVICES</a></li>
+        <li><a href="#portfolio">OUR WORK</a></li>
+        <li><a href="#pricing">PRICING</a></li>
+        <li><a href="#contact">CONTACT</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
+<!----------------------------------------------navigation over--------------------->
+<div class="jumbotron text-center">
+	<h1>Infamous Cloud <i class="fa fa-cloud"></i></h1>
+    
+    <p>The Cloud against amazon</p>
+    <form class="form-inline">
+    <input type="email" class="form-control" size="50" placeholder="Email Address">
+    <button type="button" class="btn btn-primary">Join</button>
+  </form>
+</div>
+
+<!---------------------------------------------//Jumbotron---------------------->
+
+<div id="about" class="container-fluid">
+	<div class="row">
+    <div class="col-sm-8">
+  <h2>About Company </h2>
+  <h4>Cloud is Everywhere</h4> 
+  <p>Cloud computing has now become a highly demanded service or utility due to the advantages of high computing power, cheap cost of services, high performance, scalability, accessibility as well as availability.and we make it so easy to do cloud </p>
+  <button class="btn btn-info btn-lg">Touch to Experience!</button>
+  
+  </div>
+  <div class="col-sm-4">
+  	<i class="fa fa-cloud logo"></i>
+  </div><!--//row-->
+</div><!------------ ------------------------------------container luid1--------->
+
+
+
+<div class="container-fluid bg-grey">
+
+<div class="row">
+<div class="col-sm-4">
+	<i class=" glyphicon glyphicon-fire logo"></i>
+</div>
+<div class="col-sm-8">
+<h2>How are we unique</h2>
+<h4><strong>MISSION :</strong>To make use of cloud easy to use for everyday users</h4>
+<p><strong>PS:</strong>It's so easy to do cloud with us</p>
+</div>
+</div><!--//row-->
+</div><!------------------------------------------------//container2---------------------->
+
+
+<div id="services" class="container-fluid text-center">
+<h2>SERVICES</h2>
+<h4>What we do we offer</h4>
+
+<br>
+  <div class="row">
+    <div class="col-sm-3">
+      <i class=" glyphicon glyphicon-hdd logo-small"></i>
+      <h4>StorageBox</h4>
+      <p>Store Everything.Access anywhere</p>
+    </div>
+    <div class="col-sm-3">
+      <i class="fa fa-code logo-small"></i>
+      <h4>Applications</h4>
+      <p>Enterprise Applicaions on pay per use</p>
+    </div>
+    <div class="col-sm-3">
+      <i class="fa fa-desktop logo-small"></i>
+      <h4>Inctances</h4>
+      <p>Your Operating systems</p>
+    </div>
+    <div class="col-sm-3 ">
+      <i class=" glyphicon glyphicon-tasks logo-small"></i>
+      <h4>EzCluster</h4>
+      <p>Your own Hadoop Cluster</p>
+    </div>
+    </div><!---------------- row------>
+     
+     <br><br>
+ 
+  <div class="row">
+  <div class="col-sm-3">
+      <span class=" fa fa-shield logo-small"></span>
+      <h4>Speed</h4>
+      <p>Experience the ultimate speed</p>
+    </div>
+    <div class="col-sm-3">
+      <span class="glyphicon glyphicon-leaf logo-small"></span>
+      <h4>Green</h4>
+      <p>Everything digital means an less use of paper</p>
+    </div>
+    <div class="col-sm-3">
+      <span class="glyphicon glyphicon-certificate logo-small"></span>
+      <h4>Security</h4>
+      <p>Cloud was never so secure</p>
+    </div>
+    <div class="col-sm-3">
+      <span class="glyphicon glyphicon-check logo-small"></span>
+      <h4>Up to date</h4>
+      <p>All the Softwares and OS are up to date.</p>
+    </div>
+  </div><!--------------row-------->
+
+</div><!--------------- -----------------services container------------------>
+
+
+<div id="portfolio" class="container-fluid text-center bg-grey">
+  <h2>Take a look</h2>
+  <h4>What we have created</h4>
+  <div class="row text-center">
+    <div class="col-sm-3">
+      <div class="thumbnail">
+        <img src="images/instances.jpg" alt="Paris">
+        <p><strong>Software</strong></p>
+        <p>Yes, it is on demand</p>
+      </div>
+    </div>
+    <div class="col-sm-3">
+      <div class="thumbnail">
+        <img src="images/instances.jpg" alt="New York">
+        <p><strong>Storage Box</strong></p>
+        <p>All Files are here. </p>
+      </div>
+    </div>
+    <div class="col-sm-3">
+      <div class="thumbnail">
+        <img src="images/instances.jpg" alt="San Francisco">
+        <p><strong>Instance</strong></p>
+        <p>Running OS on your browser</p>
+      </div>
+     </div>
+     <div class="col-sm-3">
+      <div class="thumbnail">
+        <img src="images/cluster.jpg" alt="San Francisco">
+        <p><strong>Instance</strong></p>
+        <p>Running OS on your browser</p>
+      </div>
+     </div>
+     
+    </div>
+</div>
+<!-------------------------------------------- thumbnails container-------------------->
+
+
+<div id="pricing" class="container-fluid">
+  <div class="text-center">
+    <h2>Pricing</h2>
+    <h4>Choose a payment plan that works for you</h4>
+  </div>
+  <div class="row">
+    <div class="col-sm-4">
+      <div class="panel panel-default text-center">
+        <div class="panel-heading">
+          <h1>Basic</h1>
+        </div>
+        <div class="panel-body">
+          <p><strong>20 GB</strong> Storage</p>
+          <p> </p>
+          <p></p>
+          <p></p>
+          <p></p>
+        </div>
+        <div class="panel-footer">
+          <h3><i class="fa  fa-inr"></i>50</h3>
+          <h4>per month</h4>
+          <button class="btn btn-lg">Sign Up</button>
+        </div>
+      </div> 
+    </div> 
+    <div class="col-sm-4">
+      <div class="panel panel-default text-center">
+        <div class="panel-heading">
+          <h1>Pro</h1>
+        </div>
+        <div class="panel-body">
+          <p><strong>50 GB</strong> Storage</p>
+          
+        </div>
+        <div class="panel-footer">
+          <h3><i class="fa fa-inr"></i>250</h3>
+          <h4>per month</h4>
+          <button class="btn btn-lg">Sign Up</button>
+        </div>
+      </div> 
+    </div> 
+   <div class="col-sm-4">
+      <div class="panel panel-default text-center">
+        <div class="panel-heading">
+          <h1>Premium</h1>
+        </div>
+        <div class="panel-body">
+          <p><strong>250 GB</strong> storage</p>
+          
+        </div>
+        <div class="panel-footer">
+          <h3><i class="fa fa-inr"></i>600</h3>
+          <h4>per month</h4>
+          <button class="btn btn-lg">Sign Up</button>
+        </div>
+      </div> 
+    </div> 
+  </div>
+</div>
+
+<!--------------------------------------------pricing corner------------------------------>
+
+
+<footer class="container-fluid text-center">
+  <a href="#myPage" title="To Top">
+    <span class="glyphicon glyphicon-chevron-up"></span>
+  </a>
+   
+</footer>
+</body>
+</html>
